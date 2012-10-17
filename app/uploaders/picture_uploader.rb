941f9cafcf8f379cb01b0702126c7131b347d8e3
jmcmains <jmcmains@gmail.com>
2012-10-17 16:36:40 -0400

    fixing image issue

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -22,6 +22,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 	def extension_white_list
      %w(jpg jpeg gif png)
   end
+  def default_url
+    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
