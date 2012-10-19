1c3f7f67efd5bf0992321f3d60bc584dd986e2ac
jmcmains <jmcmains@gmail.com>
2012-10-19 13:00:34 -0400

    change desc dt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121011162520) do
+ActiveRecord::Schema.define(:version => 20121019165820) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -49,9 +49,9 @@ ActiveRecord::Schema.define(:version => 20121011162520) do
   create_table "products", :force => true do |t|
     t.string   "name"
     t.float    "price"
-    t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.text     "description", :limit => 255
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
   create_table "related_products", :force => true do |t|
