ec13cafaf4bc6922aed1dbb2f052fec4d2aae2ef
jmcmains <jmcmains@gmail.com>
2012-11-13 11:11:38 -0500

    listing categories

--- app/controllers/category_products_controller.rb
+++ app/controllers/category_products_controller.rb
@@ -2,9 +2,8 @@ class CategoryProductsController < ApplicationController
 	def create
 		@product = Product.find(params[:product_category][:product_id])
 		@category = Category.find(params[:product_category][:category_id])
-		@category.category_products.create!(:product_id =>params[:product_category][:product_id] )
+		@category.category_products.create!(product_id: params[:product_category][:product_id] )
 		respond_to do |format|
-			format.html { redirect_to @category }
 			format.js
 		end
 	end
@@ -12,9 +11,8 @@ class CategoryProductsController < ApplicationController
 	def destroy
 		@product = Product.find(params[:product_category][:product_id])
 		@category = Category.find(params[:product_category][:category_id])
-		@product.product_categories.find_by_category_id(@category).destroy
+		ProductCategory.find_by_category_id_and_product_id(@category,@product).destroy
 		respond_to do |format|
-			format.html { redirect_to @category }
 			format.js
 		end
 	end
